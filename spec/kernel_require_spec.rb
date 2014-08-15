95fa4c27122f700f9daf1c05b72cca0a1c30a327
Marie Markwell <me@marie.so>
2014-08-15 15:05:37 -0400

    remove duplicate tests.

--- spec/kernel_require_spec.rb
+++ spec/kernel_require_spec.rb
@@ -5,24 +5,17 @@ require 'spec_helper'
 SIMPLE_RB = File.join(File.dirname(__FILE__), 'data', 'simple.rb')
 
 describe 'require' do
-  iterations = {}
+  subject = require(SIMPLE_RB, true)
 
-  iterations['first']  = require(SIMPLE_RB, true)
-  iterations['second'] = require(SIMPLE_RB, true)
+  describe 'the returned Module' do
+    it 'should populate constants' do
+      subject::Greeter.is_a?(Class)
+      subject::FooBar.is_a?(Module)
+    end
 
-  iterations.each do |key, value|
-    describe "#{key} time requiring a file" do
-      describe 'the returned Module' do
-        it 'should populate constants' do
-          value::Greeter.is_a?(Class)
-          value::FooBar.is_a?(Module)
-        end
-
-        it 'should create snake_case references to the constants' do
-          value.greeter.is_a?(Class)
-          value.foo_bar.is_a?(Module)
-        end
-      end
+    it 'should create snake_case references to the constants' do
+      subject.greeter.is_a?(Class)
+      subject.foo_bar.is_a?(Module)
     end
   end
 end
