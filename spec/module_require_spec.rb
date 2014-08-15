80c3375f031dd7c0b38a3204eb2192cdffdc16e9
Marie Markwell <me@marie.so>
2014-08-15 14:00:48 -0400

    >.>

--- spec/module_require_spec.rb
+++ spec/module_require_spec.rb
@@ -1,30 +1,3 @@
 require 'spec_helper'
 
-SIMPLE_RB = File.join(File.dirname(__FILE__), 'data', 'simple.rb')
 
-describe 'require' do
-  iterations = {}
-
-  iterations['first']  = require(SIMPLE_RB, true)
-  iterations['second'] = require(SIMPLE_RB, true)
-
-  iterations.each do |key, value|
-    describe "#{key} time requiring a file" do
-      describe 'the returned Module' do
-        it 'should populate constants' do
-          value::Greeter.is_a?(Class)
-          value::FooBar.is_a?(Module)
-        end
-
-        it 'should create snake_case references to the constants' do
-          value.greeter.is_a?(Class)
-          value.foo_bar.is_a?(Module)
-        end
-      end
-    end
-  end
-
-  it 'only loads files the first time' do
-    $times_loaded.must_equal 1
-  end
-end
