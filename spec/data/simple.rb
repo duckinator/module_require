7787d664ff7c9300e88d146b0eceef46a87ec663
Marie Markwell <me@marie.so>
2014-08-15 13:45:34 -0400

    specs!

---
+++ spec/data/simple.rb
@@ -0,0 +1,14 @@
+$times_loaded ||= 0
+$times_loaded += 1
+
+class Greeter
+  def greet(person='world')
+    "Hello, #{person}!"
+  end
+end
+
+module FooBar
+  def self.beep
+    'boop'
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+require 'module_require'
+
+require 'minitest/autorun'
+require 'minitest/spec'
